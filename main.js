05950d8b184e1574370f08ce6fcf5a1caa93bfb1
El <94628041+elelelelelel@users.noreply.github.com>
2022-09-21 13:05:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -14,13 +14,27 @@ function main() {
   //   window.addEventListener("resize", resizer);
 
   // var vertices = [x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6];
-   var vertices = [
-    -0.25, 0.8, 0.0,
-    -0.5, 0.8, 0.0,
-    -0.5, 0.5, 0.0,
-    -0.25, 0.5, 0.0,
-    -0.25, 0.2, 0.0,
-    -0.5, 0.2, 0.0]
+  var vertices1 = [-0.8, -0.8, -0.8, 0.8, -0.6, -0.8, -0.6, -0.8, -0.8, 0.8, -0.6, 0.8];
+  var vertices2 = [-0.6, 0, -0.6, 0.8, -0.4, 0, -0.4, 0, -0.6, 0.8, -0.4, 0.8];
+  var vertices3 = [-0.5, -0.8, -0.6, 0, -0.4, -0.8, -0.4, -0.8, -0.6, 0, -0.5, 0];
+  var vertices4 = [-0.2, -0.8, -0.2, 0.8, -0, -0.8, 0, -0.8, -0.2, 0.8, 0, 0.8];
+  var vertices5 = [0.5, 0.8,
+    0.1, 0.8,
+     0.1, 0, 
+     0.5, 0.0, 
+     0.5, -0.8, 
+     0.1, -0.8
+    ];
+  // var vertices6 = [0.4, -0.8, 0.4, 0, 0.5, -0.8, 0.5, -0.8, 0.4, 0, 0.5, 0];
+  // var vertices7 = [0.1, -0.8, 0.1, -0.7, 0.5, -0.8, 0.5, -0.8, 0.1, -0.7, 0.5, -0.7];
+  var vertices6 = [0.95, 0.8,
+  0.6, 0.8,
+  0.6, -0.8,
+  0.95, -0.8,
+  0.95, 0,
+  0.6, 0, 
+
+ ];
 
   // buffer itu kayak pointernya. lokasi yg di gpu nya disimpan di buffer (?, cmiiw)
   var buffer = gl.createBuffer();
@@ -53,7 +67,7 @@ function main() {
     //klo kita mau make float, kita harus pake precision
       void main(){ 
         float r = 0.0;
-        float g = 0.0;
+        float g = 1.0;
         float b = 1.0;
         gl_FragColor = vec4(r, g, b, 1.0);
       }
@@ -70,13 +84,9 @@ function main() {
 
   // kita ngajarin GPU gimana caranya mengoleksi
   // nilai posisi dari ARRAY_BUFFERuntuk setiap verteks yang sedang diproses
-  gl.clearColor(1.0, 0.65, 0.0, 1.0); //(merah, hijau, biru, transparansi)
+  gl.clearColor(1.0, 0.5, 1.0, 1.0); //(merah, hijau, biru, transparansi)
   var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-  var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 0.0, 0.0);
-
-    gl.enableVertexAttribArray(aPosition)
-    gl.drawArrays(gl.LINE_STRIP, 0, 6);
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
   //parameter stride (1 strade tu bisa mencakup banyak elemen. skrg datanya cuman 1, jd kita pake 0. g ngerti y? gpp)
   gl.enableVertexAttribArray(aPosition);
 
@@ -88,137 +98,29 @@ function main() {
   // gl.drawArrays(gl.LINES_LOOP, 0, 3);
   // klo LINES_LOOP, 1 garis dari A ke B, 1 Garis lagi dari B ke C, 1 garis lagi dari C ke A, dia nge loop
   // gl.drawArrays(gl.TRIANGLES, 0, 4);
-  gl.drawArrays(gl.LINE_STRIP, 0, 6);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices2), gl.STATIC_DRAW);
-  gl.drawArrays(gl.LINE_STRIP, 0, 6);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices3), gl.STATIC_DRAW);
-  gl.drawArrays(gl.LINE_STRIP, 0, 6);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices4), gl.STATIC_DRAW);
-  gl.drawArrays(gl.LINE_STRIP, 0, 6);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices5), gl.STATIC_DRAW);
   gl.drawArrays(gl.LINE_STRIP, 0, 6);
 
+  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices6), gl.STATIC_DRAW);
+  // gl.drawArrays(gl.LINE_STRIP, 0, 6);
+
+  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices7), gl.STATIC_DRAW);
+  // gl.drawArrays(gl.LINE_STRIP, 0, 6);
+
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices6), gl.STATIC_DRAW);
-  gl.drawArrays(gl.LINE_STRIP, 0, 6);
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices7), gl.STATIC_DRAW);
-  gl.drawArrays(gl.LINE_STRIP, 0, 6);
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices8), gl.STATIC_DRAW);
   gl.drawArrays(gl.LINE_STRIP, 0, 6);
   // TRIANGLE_STRIP -> 1 segitiga ABC, BCD
   // gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
   // TRIANGLE_FAN
 }
-
-// function main() {
-//     var kanvas = document.getElementById("kanvas");
-//     // var gl = kanvas.getContext("2d");
-//     var gl = kanvas.getContext("webgl");
-//     // kalo kita mau gambar di 3d, alat gambarnya webgl
-//     // gl = grafics library
-//     //harus lewat vertex(posisi) dulu baru bisa ke fragment(warna)
-
-//     var vertices1 = [-0.8, -0.8, -0.8, -0.8, -0.6, -0.8, -0.6, -0.8, -0.8, 0.8, -0.6, -0.8];
-//     var vertices2 = [-0.6, 0, -0.6, 0.8, -0.4, 0, -0.4, 0, -0.6, 0.8, -0.4, 0.8];
-//     var vertices3 = [-0.5, -0.8, -0.6, 0, -0.4, -0.8, -0.4, -0.8, -0.6, 0, -0.5, 0];
-//     var vertices4 = [-0.2, -0.8, -0.2, 0.8, -0, -0.8, 0, -0.8, -0.2, 0.8, 0, 0.8];
-//     var vertices5 = [0.1, 0, 0.1, 0.8, 0.5, 0, 0.5, 0, 0.1, 0.8, 0.5, 0.8];
-//     var vertices6 = [0.4, -0.8, 0.4, 0, 0.5, -0.8, 0.5, -0.8, 0.4, 0, 0.5, 0];
-//     var vertices7 = [0.1, -0.8, 0.1, -0.7, 0.5, -0.8, 0.5, -0.8, 0.1, -0.7, 0.5, -0.7];
-//     var vertices8 = [0.6, -0.8, 0.6, 0.8, 0.95, -0.8, 0.95, -0.8, -0.6, 0.8, 0.95, 0.8];
-
-//     var buffer = gl.createBuffer();
-//     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-//     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices1), gl.STATIC_DRAW);
-//     // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices2), gl.STATIC_DRAW);
-//     // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices3), gl.STATIC_DRAW);
-//     // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices4), gl.STATIC_DRAW);
-//     // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices5), gl.STATIC_DRAW);
-//     // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices6), gl.STATIC_DRAW);
-//     // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices7), gl.STATIC_DRAW);
-//     // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices8), gl.STATIC_DRAW);
-//     // mendifinisikan shaders
-//     // shaders itu ada macem2.
-//     // shaders -> sebuah source code yg akan di-run oleh gpu
-
-//     // vertex shader
-//     var vertexShaderCode = `
-//     attribute vec4 aPosition;
-//     void main(){ 
-//       float x = aPosition.x;
-//       float y = aPosition.y;
-//       gl_PointSize = 32.0;
-//       gl_Position = vec4(aPosition.xy, 0.0, 1.0);
-//     }`;
-  
-//     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
-//     gl.shaderSource(vertexShaderObject, vertexShaderCode);
-//     gl.compileShader(vertexShaderObject); //sampai sini udah jadi .o
-  
-//     // itu gaya nulisnya doang yg beda y
-//     // fragment shader
-//     var fragmentShaderCode = `
-//     precision mediump float;
-//     //klo kita mau make float, kita harus pake precision
-//       void main(){ 
-//         float r = 0.0;
-//         float g = 0.0;
-//         float b = 1.0;
-//         gl_FragColor = vec4(r, g, b, 1.0);
-//       }
-//       `;
-//     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
-//     gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
-//     gl.compileShader(fragmentShaderObject); //sampai sini jadi .o
-  
-//     var shaderProgram = gl.createProgram(); // wadah dari excecutable (.exe)
-//     gl.attachShader(shaderProgram, vertexShaderObject);
-//     gl.attachShader(shaderProgram, fragmentShaderObject);
-//     gl.linkProgram(shaderProgram);
-//     gl.useProgram(shaderProgram); //kita siap untuk menggambar (scr analogi)
-
-//     // kita mengajari GPU bagaimana cara mengoleksi
-//     // nilai posisi dari ARRAY_BUFFER
-//     // untuk setiap vertex yang sedang diproses
-//     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-//     //gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, normalized, stride, offset)
-//     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0) //tu 0 karena ada 1 data doang
-//     gl.enableVertexAttribArray(aPosition);
-
-//     gl.clearColor(1.0, 0.65, 0.0, 1.0); //(merah, hijau, biru, transparansi)
-//     gl.clear(gl.COLOR_BUFFER_BIT);
-  
-//     //gl.drawArrays(gl.POINTS, 0, 3); //<=ini cara buat titik kotak
-//     //(first -> dari index brp kita mau nulis datanya. count -> kita mau gambar/render brp kali)
-//     //gl.drawArrays(gl.LINE_LOOP, 0, 3);
-//     //gl.drawArrays(gl_TRIANGLE, 0, 3);
-//     //gl.drawArrays(gl_TRIANGLE.FAN, 0, 3);
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices1), gl.STATIC_DRAW);
-//     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices2), gl.STATIC_DRAW);
-//     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices3), gl.STATIC_DRAW);
-//     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices4), gl.STATIC_DRAW);
-//     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices5), gl.STATIC_DRAW);
-//     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices6), gl.STATIC_DRAW);
-//     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices7), gl.STATIC_DRAW);
-//     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-
-//     // gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(vertices8), gl.STATIC_DRAW);
-//     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
-//   }
